50a02502f9db1b900de0a8fb5ecdb713f6d342c8
Smit Jain <smitjain.sj@gmail.com>
2016-06-28 02:18:37 +0530

    fixing site.pp

--- site.pp
+++ site.pp
@@ -1,26 +1,24 @@
 node default {
-class { 'nginx' : }
-class { 'java':
-  distribution => 'jdk',
-}
+
+	class { 'nginx' : }
+	class { 'java': distribution => 'jdk' }
+
 class { 'tomcat':
   version     => 8,
   sources     => true,
   sources_src => 'http://archive.apache.org/dist/tomcat/',
-}
-tomcat::instance {'tomcat1':
-  ensure      => present,
-  http_port   => '8080',
+	}
+	tomcat::instance {'tomcat1':
+	  ensure      => present,
+	  http_port   => '8080',
 	}
 
-exec {'tocmat':
-	command => '/bin/cp /opt/companyNews.war /opt/apache-tomcat/webapps \
-		bash /opt/apache-tomcat/bin/startup.sh ' ,
-	require => Class['tomcat'], 
-
-}
-
-}	
+	exec {'tocmat':
+		command => '/bin/cp /opt/companyNews.war /opt/apache-tomcat/webapps \
+			bash /opt/apache-tomcat/bin/startup.sh ' ,
+		require => Class['tomcat'], 
+		}
+	}	
 
 
 
