c736d33a97ddded3f025acbcd2ea65e6545aeb9d
Smit Jain <smitjain.sj@gmail.com>
2016-06-28 23:32:28 +0530

    unknown fix

--- site.pp
+++ site.pp
@@ -1,12 +1,12 @@
 node default {
 
-        class { 'nginx' : }
-        class { 'java': distribution => 'jdk' }
+        class { 'java': distribution => 'jdk' } -> class { 'nginx' : }
 
 class { 'tomcat':
   version     => 7,
   sources     => true,
   sources_src => 'http://archive.apache.org/dist/tomcat/',
+  require => Service['nginx'],
 }
 
 tomcat::instance {'companynews':
@@ -14,8 +14,13 @@ tomcat::instance {'companynews':
   server_port => '8005',
   http_port   => '8080',
   ajp_port    => '8009',
+  before => Exec['deploy'],
 }
 
+exec{'deploy':
+	command => "/bin/cp /tmp/companyNews.war /opt/apache-tomcat/webapps \
+			bash /opt/apache-tomcat/bin/startup.sh " ,
+	}
 
 }
 
