ad18d70a61ffdefa4f732c2c9745e76fca8684a4
Smit Jain <smitjain.sj@gmail.com>
2016-07-01 03:05:22 +0530

    Fixed Site.pp

--- site.pp
+++ site.pp
@@ -1,30 +1,30 @@
 node default {
 
-class { 'java': distribution => 'jdk' } -> class { 'nginx' : }
-
-
-class { 'tomcat':
+class { 'java': distribution => 'jdk' } ->
+class { 'nginx' : } ->
+class {'tomcat':
   version     => 7,
   sources     => true,
   sources_src => 'http://archive.apache.org/dist/tomcat/',
 }
+tomcat::instance {'companynews':
+  ensure      => 'present',
+  server_port => '8005',
+  http_port   => '8080',
+  ajp_port    => '8009',
+}
 
-
-file{'/opt/apache-tomcat/webapps/companyNews.war':
- ensure => 'present' ,
+file{'/srv/tomcat/companynews/webapps/companyNews.war':
+ ensure => present,
  mode => '0755',
- owner => 'root',
- group => 'root',
+ owner => 'tomcat',
+ group => 'adm',
  source =>  'puppet:///modules/tomcat/companyNews.war' ,
  }
-
 	exec{'deploy':
 		path   => '/usr/bin:/usr/sbin:/bin:/sbin',
-		command => '/bin/bash /opt/apache-tomcat*/bin/startup.sh', 
-		onlyif  =>  "/bin/ls /opt/apache-tomcat/webapps/companyNews.war" ,
-		require => File['/opt/apache-tomcat/webapps/companyNews.war'],	
+		command => '/bin/bash /srv/tomcat/companynews/bin/startup.sh', 
+		onlyif  =>  "/bin/ls /srv/tomcat/comanpynews/webapps/companyNews.war" ,
+		require => File['/srv/tomcat/companynews/webapps/companyNews.war'],	
             }
-
-
-
 }
